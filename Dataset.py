042deb1756da0f4e2f68da7ef80da1152f309813
Kausik N <nkausik1999@gmail.com>
2022-03-31 11:31:31 +0530

    added dataset, model functions - CS21M037

--- Dataset.py
+++ Dataset.py
@@ -4,15 +4,31 @@ Dataset
 
 # Imports
 import os
+import shutil
+import numpy as np
+np.random.seed(0)
+from tqdm import tqdm
+from keras.preprocessing import *
+from keras.preprocessing.image import *
 
 
 # Main Vars
-
+DATASET_PATH_INATURALIST = "Dataset/inaturalist_12K"
+DATASET_INATURALIST_CLASSES = ["Amphibia", "Animalia", "Arachnida", "Aves", "Fungi", "Insecta", "Mammalia", "Mollusca", "Plantae", "Reptilia"]
 
 # Main Functions
 # Load Train and Test Dataset Functions @ Karthikeyan S CS21M028
 
 # Get Random Image Path from Dataset @ N Kausik CS21M037
-
+def GetImagePath_Random():
+    '''
+    Get Random Image Path in train dataset
+    '''
+    dataset_path = os.path.join(DATASET_PATH_INATURALIST, "train")
+    class_random = os.listdir(dataset_path)[np.random.randint(0, 10)]
+    class_Is = os.listdir(os.path.join(dataset_path, class_random))
+    I_name = class_Is[np.random.randint(0, len(class_Is))]
+    I_path = os.path.join(dataset_path, class_random, I_name)
+    return I_path, class_random
 
 # Run
--- Model.py
+++ Model.py
@@ -3,6 +3,13 @@ Model
 '''
 
 # Imports
+import wandb
+from wandb.keras import WandbCallback
+import math
+from keras.models import Sequential, load_model
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.callbacks import ModelCheckpoint
+
 from Library.ModelBlocks import *
 from Dataset import *
 
@@ -19,7 +26,61 @@ from Dataset import *
 # Compile Model Function @ Karthikeyan S CS21M028
 
 # Train Model Function @ N Kausik CS21M037
+def Model_Train(model, inputs, n_epochs, wandb_data, **params):
+    '''
+    Train Model
+    '''
+    # Get Data
+    DATASET = inputs["dataset"]
+    TRAIN_STEP_SIZE = math.ceil(DATASET["train"].n / DATASET["train"].batch_size)
+    VALIDATION_STEP_SIZE = math.ceil(DATASET["val"].n / DATASET["val"].batch_size)
+
+    # Enable Wandb Callback
+    WandbCallbackFunc = WandbCallback(
+        monitor="val_accuracy", save_model=True, log_evaluation=True, log_weights=True,
+        log_best_prefix="best_",
+        generator=DATASET["val"], validation_steps=VALIDATION_STEP_SIZE
+    )
+    # Enable Model Checkpointing
+    ModelCheckpointFunc = ModelCheckpoint(
+        "Models/best_model.h5",
+        monitor="val_accuracy",
+        verbose=1,
+        save_best_only=True,
+        mode="max",
+        save_freq="epoch"
+    )
+
+    # Train Model
+    TRAIN_HISTORY = model.fit(
+        DATASET["train"], steps_per_epoch=TRAIN_STEP_SIZE,
+        validation_data=DATASET["val"], validation_steps=VALIDATION_STEP_SIZE,
+        epochs=n_epochs,
+        verbose=1,
+        callbacks=[WandbCallbackFunc, ModelCheckpointFunc]
+    )
+
+    return model, TRAIN_HISTORY
 
 # Test Model Function @ N Kausik CS21M037
+def Model_Test(model, dataset, **params):
+    '''
+    Test Model
+    '''
+    # Test Model
+    loss_test, eval_test = model.evaluate(dataset, verbose=1)
+
+    return loss_test, eval_test
 
 # Load and Save Model Functions @ N Kausik CS21M037
+def Model_LoadModel(path):
+    '''
+    Load Model
+    '''
+    return load_model(path)
+
+def Model_SaveModel(model, path):
+    '''
+    Save Model
+    '''
+    return model.save(path)
